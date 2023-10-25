edabb69268ef73e879d9fb009683a8488dc9a19a
Deivison Cardoso <deivisondc@gmail.com>
2023-10-25 11:55:32 -0300

    add debugger function

--- aula01-mp4demuxer-canvas-renderer/app/pages/file-upload/src/app.js
+++ aula01-mp4demuxer-canvas-renderer/app/pages/file-upload/src/app.js
@@ -16,3 +16,23 @@ view.configureOnFileChange((file) => {
     view.updateElpasedTime(`Process took ${took.replace("ago", "")}`);
   }, 5000);
 });
+
+async function fakeFetch() {
+  const filePath = "/videos/frag_bunny.mp4";
+  const response = await fetch(filePath);
+  //   const response = await fetch(filePath, {
+  //     method: "HEAD", // not necessary.. head made possible to use response.headers.get('content-length') to get file size
+  //   });
+
+  const file = new File([await response.blob()], filePath, {
+    type: "video/mp4",
+    lastModified: Date.now(),
+  });
+
+  const event = new Event("change");
+  Reflect.defineProperty(event, "target", { value: { files: [file] } });
+
+  document.getElementById("fileUpload").dispatchEvent(event);
+}
+
+fakeFetch();
